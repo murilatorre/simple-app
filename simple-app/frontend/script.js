4f22c35b926db93464f8ca04e909f541935e1b56
murilatorre <135985974+murilatorre@users.noreply.github.com>
2025-05-14 17:13:20 -0300

    Delete simple-app directory

--- simple-app/frontend/script.js
+++
@@ -1,9 +0,0 @@
-fetch('/api/mensagem')
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById('mensagem').textContent = data.mensagem;
-  })
-  .catch(err => {
-    document.getElementById('mensagem').textContent = 'Erro ao carregar mensagem.';
-    console.error(err);
-  });
